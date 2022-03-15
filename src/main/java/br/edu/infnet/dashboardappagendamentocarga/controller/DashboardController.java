2c0291d2d1484e92085534a952d36d46e0764f58
Lu1zF3rnand0 <luizfernando.silva@outlook.com>
2022-03-15 20:54:25 -0300

    Ajustes

--- src/main/java/br/edu/infnet/dashboardappagendamentocarga/controller/DashboardController.java
+++ src/main/java/br/edu/infnet/dashboardappagendamentocarga/controller/DashboardController.java
@@ -9,11 +9,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import br.edu.infnet.dashboardappagendamentocarga.model.domain.Usuario;
 import br.edu.infnet.dashboardappagendamentocarga.service.AgendamentoService;
-import br.edu.infnet.dashboardappagendamentocarga.service.CargaSoltaService;
-import br.edu.infnet.dashboardappagendamentocarga.service.ConteinerService;
 import br.edu.infnet.dashboardappagendamentocarga.service.LogService;
 import br.edu.infnet.dashboardappagendamentocarga.service.UsuarioService;
-import br.edu.infnet.dashboardappagendamentocarga.service.VeiculoService;
 
 @Controller
 public class DashboardController {
@@ -25,34 +22,17 @@ public class DashboardController {
 	private AgendamentoService agendamentoService;
 	
 	@Autowired
-	private ConteinerService conteinerService;
-
-	@Autowired
-	private VeiculoService veiculoService;
-
-	@Autowired
-	private CargaSoltaService cargaSoltaService;
-
-	@Autowired
 	private LogService logService;
 
 	@GetMapping(value = "/")
 	public String index(Model model) {
 
-		//produtos e pedidos por usuário
 		List<Usuario> usuarios = usuarioService.obterLista();		
 		model.addAttribute("agendamentosPorUsuario", usuarios);		
 		model.addAttribute("cargasPorUsuario", usuarios);
 		
-		//quantidade de pedidos
 		model.addAttribute("qtdeAgendamentos", agendamentoService.obterLista().size());
 		
-//		//valor de vendos dos produtos
-//		model.addAttribute("valorVendaBebida", bebidaService.calcularValorVenda());
-//		model.addAttribute("valorVendaComida", comidaService.calcularValorVenda());
-//		model.addAttribute("valorVendaSobremsa", sobremesaService.calcularValorVenda());
-
-		//recuperar o log
 		model.addAttribute("listaLog", logService.obterLista());
 		model.addAttribute("qtdeLog", logService.obterQuantidade());
 		
